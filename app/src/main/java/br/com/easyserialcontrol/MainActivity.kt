99b94716117e7f4be221da605d1b0acb0452c5b1
LeonardoRKS <leonardo.camargo.ti@gmail.com>
2024-01-02 15:23:00 -0300

    fist commit

---
+++ app/src/main/java/br/com/easyserialcontrol/MainActivity.kt
@@ -0,0 +1,235 @@
+package br.com.easyserialcontrol
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothAdapter.getDefaultAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Build.VERSION.SDK_INT
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import br.com.easyserialcontrol.ui.theme.EasySerialControlTheme
+
+
+/*data class BluetoothModel(
+    val name: String
+)*/
+
+class MainActivity : ComponentActivity() {
+    private val PERMISSION_CODE = 1
+
+    private val bluetoothAdapter: BluetoothAdapter by lazy {
+        val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        bluetoothManager.adapter
+    }
+
+    private val activityResultLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            Log.i("Bluetooth", ":request permission result ok")
+        } else {
+            Log.i("Bluetooth", ":request permission result canceled /denied")
+        }
+    }
+
+    private fun requestBluetoothPermission() {
+        val enableBluetoothIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+        activityResultLauncher.launch(enableBluetoothIntent)
+    }
+
+//    var pairedDevices: Set<BluetoothDevice> = bluetoothAdapter.bondedDevices
+    var discorveredDevices: Set<BluetoothDevice> = emptySet()
+
+    private val receiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent) {
+            when (intent.action) {
+                BluetoothDevice.ACTION_FOUND -> {
+                    val device: BluetoothDevice? =
+                        intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
+                    if (device != null) {
+                        val updated = discorveredDevices.plus(device)
+                        discorveredDevices = updated
+                    }
+                    Log.i("Bluetooth", "onReceive: Device found")
+                }
+
+                BluetoothAdapter.ACTION_DISCOVERY_STARTED -> {
+                    Log.i("Bluetooth", "onReceive: Started Discovery")
+                }
+
+                BluetoothAdapter.ACTION_DISCOVERY_FINISHED -> {
+                    Log.i("Bluetooth", "onReceive: Finished Discovery")
+                }
+            }
+        }
+    }
+
+    private fun scan(): Set<BluetoothDevice> {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.BLUETOOTH_SCAN
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            if (bluetoothAdapter.isDiscovering) {
+                bluetoothAdapter.cancelDiscovery()
+                bluetoothAdapter.startDiscovery()
+            } else {
+                bluetoothAdapter.startDiscovery()
+            }
+
+            Handler(Looper.getMainLooper()).postDelayed({
+                bluetoothAdapter.cancelDiscovery()
+            }, 10000L)
+        }
+
+        return discorveredDevices
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val foundFilter = IntentFilter(BluetoothDevice.ACTION_FOUND)
+        val startFilter = IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_STARTED)
+        val endFilter = IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED)
+        registerReceiver(receiver, foundFilter)
+        registerReceiver(receiver, startFilter)
+        registerReceiver(receiver, endFilter)
+
+        if (!bluetoothAdapter.isEnabled) {
+            requestBluetoothPermission()
+        }
+
+        if (SDK_INT >= Build.VERSION_CODES.O) {
+            if (ContextCompat.checkSelfPermission(
+                    baseContext, android.Manifest.permission.ACCESS_BACKGROUND_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this, arrayOf(android.Manifest.permission.ACCESS_BACKGROUND_LOCATION),
+                    PERMISSION_CODE
+                )
+            }
+        }
+
+
+        setContent {
+            var devices: Set<BluetoothDevice> by remember { mutableStateOf(emptySet()) }
+
+            EasySerialControlTheme {
+                // A surface container using the 'background' color from the theme
+                Surface() {
+                    Scaffold(
+                        topBar = {
+                            TopAppBar(
+                                title = {
+                                    Text(
+                                        text = "Bluetooth Connected List",
+                                        modifier = Modifier.fillMaxWidth(),
+                                        textAlign = TextAlign.Center
+                                    )
+                                }
+                            )
+                        }
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(top = 20.dp),
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Button(
+                                onClick = { devices = scan() }
+                            ) {
+                                Text(
+                                    text = "Scan",
+                                    style = MaterialTheme.typography.bodyMedium
+                                )
+                            }
+                            Spacer(modifier = Modifier.height(10.dp))
+                            Text(
+                                text = "Paired Devices",
+                                style = MaterialTheme.typography.bodySmall
+                            )
+                            Spacer(modifier = Modifier.height(10.dp))
+                            val pairedDevices: Set<BluetoothDevice> = bluetoothAdapter.bondedDevices
+                            pairedDevices.forEach { device ->
+                                Card(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(horizontal = 10.dp, vertical = 5.dp),
+                                    elevation = CardDefaults.cardElevation(10.dp)
+                                ) {
+                                    Column(
+                                        modifier = Modifier
+                                            .fillMaxWidth()
+                                            .padding(10.dp),
+                                        verticalArrangement = Arrangement.spacedBy(10.dp)
+                                    ) {
+                                        Text(text = device.name)
+                                        Text(text = device.address)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.BLUETOOTH_SCAN
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            if (bluetoothAdapter.isDiscovering)
+                bluetoothAdapter.cancelDiscovery()
+        }
+
+        unregisterReceiver(receiver)
+
+    }
+}
